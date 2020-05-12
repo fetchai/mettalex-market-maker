1378735b38910404401574ab7f0f5a652a2a3468
mattmcd <matt@matt-mcdonnell.com>
2020-05-12 11:34:29 +0100

    CBTF-173 Start position tokens with zero total balance.  Fix transfer call in python (gas estimate error)

--- on-chain/mettalex-protocol/contracts/PositionToken.sol
+++ on-chain/mettalex-protocol/contracts/PositionToken.sol
@@ -183,7 +183,7 @@ contract PositionToken is StandardToken {
     string public name;
     string public symbol;
     uint8 public decimals = 18;
-    uint public totalSupply = 1560000000 * 10**18;
+    uint public totalSupply = 0;
 
     address public owner;
     mapping(address => bool) public whitelist;
