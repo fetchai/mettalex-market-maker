22f8367b76d099a2f51758a5664fd9063c37afc4
Ayush Tiwari <ayusht11@outlook.com>
2020-08-14 19:53:07 +0530

    MEX-151: updated truffle config

--- on-chain/mettalex-protocol/truffle-config.js
+++ on-chain/mettalex-protocol/truffle-config.js
@@ -19,10 +19,12 @@
  */
 
 module.exports = {
-  development: {
-    host: '127.0.0.1', // Localhost (default: none)
-    port: 9545, // Standard Ethereum port (default: none)
-    network_id: '*', // Any network (default: none)
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*',
+    },
   },
-  plugins: ["solidity-coverage"]
+  plugins: ['solidity-coverage'],
 };
