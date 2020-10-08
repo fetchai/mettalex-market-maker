410d8c32ae3736aba47a7759082fd957b131680f
Ankit Mahajan <ankit.mahajan@fetch.ai>
2020-10-08 19:27:22 +0530

    Added script to compile all the contracts

---
+++ on-chain/compile.sh
@@ -0,0 +1,14 @@
+cd mettalex-balancer
+npx oz compile
+
+cd ../mettalex-coin
+npx oz compile
+
+cd ../mettalex-vault
+npx oz compile
+
+cd ../mettalex-yearn
+npx oz compile
+
+cd ../pool-controller
+npx oz compile
