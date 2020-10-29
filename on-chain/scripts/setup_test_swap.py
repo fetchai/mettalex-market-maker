9ee89e87516a38df1eee1e86f1e2dae9ea51eb5d
Ankit Mahajan <ankit.mahajan@fetch.ai>
2020-10-29 17:53:11 +0530

    Updated swap test script

--- on-chain/scripts/setup_test_swap.py
+++ on-chain/scripts/setup_test_swap.py
@@ -1,95 +1,120 @@
-# This is a helper file to test breach functionality on Python console
-from deploy_contracts import connect, deploy, get_contracts, mintPositionTokens, swap, full_setup, deposit, earn, upgrade_strategy, BalanceReporter, connect_deployed, withdraw, connect_strategy, deploy_contract, get_contracts, whitelist_vault, get_balance
-
-# setup
-w3, admin = connect('local', 'admin')
-contracts = get_contracts(w3)
-deployed_contracts = deploy(w3, contracts)
-w3, acc, deployed_contracts = full_setup(w3, admin, deployed_contracts=deployed_contracts, price=2500)
-
-coin = deployed_contracts['Coin']
-ltk = deployed_contracts['Long']
-stk = deployed_contracts['Short']
-vault = deployed_contracts['Vault']
-bpool = deployed_contracts['BPool']
-y_vault = deployed_contracts['YVault']
-y_controller = deployed_contracts['YController']
-strategy = deployed_contracts['PoolController']
-
-#Bind tokens to BPool
-deposit(w3, y_vault, coin, 10000000)
-earn(w3, y_vault)
-
-mintPositionTokens(w3, vault, coin, 100000000, admin)
+from deploy_contracts import connect, deploy, get_contracts, get_spot_price, mintPositionTokens, swap, full_setup, deposit, earn, upgrade_strategy, connect_deployed, withdraw, connect_strategy, deploy_contract, get_contracts, whitelist_vault, get_balance
 
 
-#initial Pool Balance:
-print('Initial Pool Balance:')
-stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
+def swap_from_coin():
+    print('Coin to Short swap:')
+    swap(w3, strategy, coin, 1000000000, stk, 1)
+    stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
 
-print('User Balance:')
-stk_balance, ltk_balance, coin_balance = get_balance(admin, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
+    print('Spot prices: ')
+    print('Long: ', get_spot_price(w3, bpool, coin, ltk, unitless=False))
+    print('Short: ', get_spot_price(w3, bpool, coin, stk, unitless=False))
+    print('Total Positions: ')
+    total_position_tokens = (stk_balance + ltk_balance)
+    print(f'Coin: {coin_balance} and Positions: {total_position_tokens}')
 
+    print('Coin to Long swap:')
+    swap(w3, strategy, coin, 1000000000, ltk, 1)
+    stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
 
-
-# Swap Coin to Positions
-swap(w3, strategy, coin, 1000000000, stk, 1)
-print('Pool Balance after Coin to Short swap:')
-stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
-print('User Balance:')
-stk_balance, ltk_balance, coin_balance = get_balance(admin, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
-
-
-
-swap(w3, strategy, coin, 1000000000, ltk, 1)
-print('Pool Balance after Coin to Long swap:')
-stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
-print('User Balance:')
-stk_balance, ltk_balance, coin_balance = get_balance(admin, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
+    print('Spot prices: ')
+    print('Long: ', get_spot_price(w3, bpool, coin, ltk, unitless=False))
+    print('Short: ', get_spot_price(w3, bpool, coin, stk, unitless=False))
+    print('Total Positions: ')
+    total_position_tokens = (stk_balance + ltk_balance)
+    print(f'Coin: {coin_balance} and Positions: {total_position_tokens}')
 
 
+def swap_to_coin():
+    print('Short to Coin swap:')
+    swap(w3, strategy, stk, 100000, coin, 1)
+    stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
 
-# Swap Positions to Coin
-swap(w3, strategy, stk, 100000, coin, 1)
-print('Pool Balance after Short to Coin swap:')
-stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
-print('User Balance:')
-stk_balance, ltk_balance, coin_balance = get_balance(admin, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
-
-swap(w3, strategy, ltk, 100000, coin, 1)
-print('Pool Balance after Long to Coin swap:')
-stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
-print('User Balance:')
-stk_balance, ltk_balance, coin_balance = get_balance(admin, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
+    print('Spot prices: ')
+    print('Long: ', get_spot_price(w3, bpool, coin, ltk, unitless=False))
+    print('Short: ', get_spot_price(w3, bpool, coin, stk, unitless=False))
+    print('Total Positions: ')
+    total_position_tokens = (stk_balance + ltk_balance)
+    print(f'Coin: {coin_balance} and Positions: {total_position_tokens}')
 
 
+    print('Long to Coin swap:')
+    swap(w3, strategy, ltk, 100000, coin, 1)
+    stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
 
-# Swap Position to Position
+    print('Spot prices: ')
+    print('Long: ', get_spot_price(w3, bpool, coin, ltk, unitless=False))
+    print('Short: ', get_spot_price(w3, bpool, coin, stk, unitless=False))
+    print('Total Positions: ')
+    total_position_tokens = (stk_balance + ltk_balance)
+    print(f'Coin: {coin_balance} and Positions: {total_position_tokens}')
+
 
 ### Swap works for 10,000 or more STK/LTK
+def swap_positions():
+    print('Long to Short swap:')
+    swap(w3, strategy, ltk, 1000000000, stk, 1)
+    stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
 
-swap(w3, strategy, ltk, 1000000000, stk, 1)
-print('Pool Balance after Long to Short swap:')
-stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
-print('User Balance:')
-stk_balance, ltk_balance, coin_balance = get_balance(admin, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
+    print('Spot prices: ')
+    print('Long: ', get_spot_price(w3, bpool, coin, ltk, unitless=False))
+    print('Short: ', get_spot_price(w3, bpool, coin, stk, unitless=False))
+    print('Total Positions: ')
+    total_position_tokens = (stk_balance + ltk_balance)
+    print(f'Coin: {coin_balance} and Positions: {total_position_tokens}')
 
-swap(w3, strategy, stk, 1000000000, ltk, 1)
-print('Pool Balance after Short to Long swap:')
-stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
-print('User Balance:')
-stk_balance, ltk_balance, coin_balance = get_balance(admin, coin, ltk, stk)
-print(f'Coin: {coin_balance}, Long: {ltk_balance} and Short: {stk_balance} \n')
+
+    print('Short to Long swap:')
+    swap(w3, strategy, stk, 1000000000, ltk, 1)
+    stk_balance, ltk_balance, coin_balance = get_balance(bpool.address, coin, ltk, stk)
+
+    print('Spot prices: ')
+    print('Long: ', get_spot_price(w3, bpool, coin, ltk, unitless=False))
+    print('Short: ', get_spot_price(w3, bpool, coin, stk, unitless=False))
+    print('Total Positions: ')
+    total_position_tokens = (stk_balance + ltk_balance)
+    print(f'Coin: {coin_balance} and Positions: {total_position_tokens}')
+
+
+if __name__ == '__main__':
+    # setup
+    w3, admin = connect('local', 'admin')
+    contracts = get_contracts(w3)
+    deployed_contracts = deploy(w3, contracts)
+    w3, acc, deployed_contracts = full_setup(w3, admin, deployed_contracts=deployed_contracts, price=2500)
+
+    coin = deployed_contracts['Coin']
+    ltk = deployed_contracts['Long']
+    stk = deployed_contracts['Short']
+    vault = deployed_contracts['Vault']
+    bpool = deployed_contracts['BPool']
+    y_vault = deployed_contracts['YVault']
+    y_controller = deployed_contracts['YController']
+    strategy = deployed_contracts['PoolController']
+    
+    #Bind tokens to BPool
+    deposit(w3, y_vault, coin, 10000000)
+    earn(w3, y_vault)
+    mintPositionTokens(w3, vault, coin, 100000000, admin)
+
+
+    #initial Pool Balance:
+    print('Initial Pool Balance:')
+    get_balance(bpool.address, coin, ltk, stk)
+    print('User Balance:')
+    get_balance(admin, coin, ltk, stk)
+
+    #swaps
+    swap_from_coin()
+    print('User Balance:')
+    get_balance(admin, coin, ltk, stk)
+
+
+    swap_to_coin()
+    print('User Balance:')
+    get_balance(admin, coin, ltk, stk)
+
+
+    swap_positions()
+    print('User Balance:')
+    get_balance(admin, coin, ltk, stk)
