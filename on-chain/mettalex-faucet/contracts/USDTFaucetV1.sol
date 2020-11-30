29715b8c23fc89da275b8924bcfcd28879b5b277
AnkitMDev <69793304+AnkitMDev@users.noreply.github.com>
2020-11-30 19:08:46 +0530

    MEX-513 faucet contract updates and deployment setup script (#6)
    * Added getters to faucet for solving eth_call in bsc
    
    * Added deployment and setup script for faucet

---
+++ on-chain/mettalex-faucet/contracts/USDTFaucetV1.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.5.0;
+
+import "./SafeMath.sol";
+import "./IUSDT.sol";
+
+contract USDTFaucetV1 {
+    using SafeMath for uint256;
+    uint256 public dailyLimit;
+    address public tokenAddress;
+    address public owner;
+    //mapping for amount withdrawn and timestamp of withdrawal
+    mapping(address => uint256) public amountWithdrawn;
+    mapping(address => uint256) public lastWithdrawnAt;
+
+    constructor(address _tokenAddress, uint256 _limit) public {
+        tokenAddress = _tokenAddress;
+        dailyLimit = _limit;
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+
+    function updateTokenAddress(address _tokenAddress) external onlyOwner {
+        tokenAddress = _tokenAddress;
+    }
+
+    function updateOwner(address _owner) external onlyOwner {
+        owner = _owner;
+    }
+
+    function updateDailyLimit(uint256 _dailyLimit) external onlyOwner {
+        dailyLimit = _dailyLimit;
+    }
+
+    function request(address user, uint256 amount) external onlyOwner {
+        require(msg.sender == owner, "Only Owner!");
+        require(amount <= dailyLimit, "Amount exceeding limit");
+        if (now.sub(lastWithdrawnAt[user]) <= 86400) {
+            require(
+                amountWithdrawn[user].add(amount) <= dailyLimit,
+                "Daily Limit Exceeded"
+            );
+        } else {
+            amountWithdrawn[user] = 0;
+            lastWithdrawnAt[user] = now;
+        }
+        IUSDT(tokenAddress).mint(user, amount);
+        amountWithdrawn[user] = amountWithdrawn[user].add(amount);
+    }
+}
