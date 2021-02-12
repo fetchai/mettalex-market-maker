102c0a15350c54c395341430464cc6db8ae8633a
Mehul Gaidhani <mehulgaidhani751@gmail.com>
2021-02-12 13:25:33 +0530

    variable name changes

--- on-chain/mettalex-bridge/contracts/bridge.sol
+++ on-chain/mettalex-bridge/contracts/bridge.sol
@@ -5,21 +5,21 @@ import "./IUSDT.sol";
 
 contract Bridge {
     using SafeMath for uint256;
-    uint256 public multiplier;
+    uint256 public conversionRate;
     uint256 public maxDeposit;
     address public owner;
     address public tokenAddress;
     address public USDTaddress;
-    uint256 public depositLimit;
+    uint256 public depositLimitMultiplier;
     mapping(address => uint256) public amountDeposited;
 
 
-    constructor(address _USDTaddress, address _tokenAddress, uint256 _multiplier, uint256 _maxDeposit, uint256 _depositLimit) public {
+    constructor(address _USDTaddress, address _tokenAddress, uint256 _conversionRate, uint256 _maxDeposit, uint256 _depositLimitMultiplier) public {
         tokenAddress = _tokenAddress;
         USDTaddress = _USDTaddress;
-        multiplier = _multiplier;
+        conversionRate = _conversionRate;
         maxDeposit = _maxDeposit;
-        depositLimit = _depositLimit;
+        depositLimitMultiplier = _depositLimitMultiplier;
         owner = msg.sender;
     }
 
@@ -28,12 +28,12 @@ contract Bridge {
         _;
     }
 
-    function setMultiplier(uint256 _multiplier)
+    function setConversionRate(uint256 _conversionRate)
         external
         onlyOwner
     {
-        require(_multiplier > 0, "Multiplier should be greater than 0");
-        multiplier = _multiplier;
+        require(_conversionRate > 0, "Multiplier should be greater than 0");
+        conversionRate = _conversionRate;
     }
 
     function setMaxDeposit(uint256 _maxDeposit)
@@ -47,8 +47,8 @@ contract Bridge {
         tokenAddress = _tokenAddress;
     }
 
-    function setDepositLimit(uint256 _limit) external onlyOwner {
-        depositLimit = _limit;
+    function setdepositLimitMultiplier(uint256 _limit) external onlyOwner {
+        depositLimitMultiplier = _limit;
     }
 
     function updateOwner(address _owner) external onlyOwner {
@@ -56,18 +56,18 @@ contract Bridge {
     }
 
     function deposit(uint256 _amountUSDT) external {
-        require(_amountUSDT > multiplier, "ERR: min deposit");
+        require(_amountUSDT > conversionRate, "ERR: min deposit");
         require(_amountUSDT <= maxDeposit, "ERR: max deposit");
-        require(amountDeposited[msg.sender].add(_amountUSDT) < maxDeposit.mul(depositLimit), "ERR: deposit limit reached");
+        require(amountDeposited[msg.sender].add(_amountUSDT) < maxDeposit.mul(depositLimitMultiplier), "ERR: deposit limit reached");
         IUSDT(USDTaddress).transferFrom(msg.sender, address(this), _amountUSDT);
         amountDeposited[msg.sender] = amountDeposited[msg.sender].add(_amountUSDT);
-        IUSDT(tokenAddress).transfer(msg.sender, _amountUSDT.mul(multiplier));
+        IUSDT(tokenAddress).transfer(msg.sender, _amountUSDT.mul(conversionRate));
     }
 
     function withdraw(uint256 _amountwUSDT) external {
         require(_amountwUSDT < IUSDT(USDTaddress).balanceOf(address(this)), "ERR: max Withdraw");
-        uint256 amountOut = _amountwUSDT.div(multiplier);
-        uint256 amountDecrease = amountOut.mul(multiplier);
+        uint256 amountOut = _amountwUSDT.div(conversionRate);
+        uint256 amountDecrease = amountOut.mul(conversionRate);
         IUSDT(tokenAddress).transferFrom(msg.sender, address(this), amountDecrease);
         amountDeposited[msg.sender] = amountDeposited[msg.sender].sub(amountDecrease);
         IUSDT(USDTaddress).transfer(msg.sender, amountOut);
