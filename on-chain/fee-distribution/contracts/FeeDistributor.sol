196df862326ae6f4b07b1d6a4492c023ebe3f5f2
Mehul Gaidhani <mehulgaidhani751@gmail.com>
2021-02-26 17:45:47 +0530

    netspec comments added

--- on-chain/fee-distribution/contracts/FeeDistributor.sol
+++ on-chain/fee-distribution/contracts/FeeDistributor.sol
@@ -24,6 +24,13 @@ contract FeeDistributor is Ownable {
     mapping (address => uint256) private currentWithdrawalDistributionId;
     uint256 public distributionIndex;
 
+    /**
+     * @dev The constructor sets initial values
+     * @param _want address The address of coin
+     * @param _strategy address The address of linked strategy
+     * @param userAddresses array of address of users that can withdraw fees
+     * @param fractions array of uint256 that will contain fractions of fees distribution
+     */
     constructor (
         address _want,
         address _strategy,
@@ -46,6 +53,13 @@ contract FeeDistributor is Ownable {
         }
     }
 
+
+    /**
+     * @dev Updates fractions for each user
+     * @param userAddresses array of address of users that can withdraw fees
+     * @param fractions array of uint256 that will contain fractions of fees distribution
+     * @return true if updation successful
+     */
     function updateFractions(
         address[] calldata userAddresses,
         uint256[] calldata fractions
@@ -60,6 +74,11 @@ contract FeeDistributor is Ownable {
         }
     }
 
+    /**
+     * @dev called externally by strategy to transfer fees to this contract
+     * @param amount uint256 the amount that has to be sent to this contract( for distribution)
+     * @return true if successful
+     */
     function sendFees(uint256 amount) external returns (bool) {
         require(amount > 0, "Invalid amount");
         require(msg.sender == strategy, "!strategy");
@@ -70,6 +89,10 @@ contract FeeDistributor is Ownable {
         return true;
     }
 
+    /**
+     * @dev called externally by user to transfer collected fees to his account
+     * @return true if updation amount transfer wrt fraction sucessful
+     */
     function withdraw() external returns (bool) {
         uint256 totalAmount = 0;
         uint256 currentUserDistId = currentWithdrawalDistributionId[msg.sender];
@@ -89,20 +112,40 @@ contract FeeDistributor is Ownable {
         return true;
     }
 
+
+    /**
+     * @dev called externally by owner to update strategy address linked to the contract
+     * @return true if updation sucessful
+     */
     function updateStrategy(address _newStrategy) external onlyOwner returns (bool) {
         require (_newStrategy != address(0) && _newStrategy != strategy);
         strategy = _newStrategy;
     }
 
+    /**
+     * @dev called externally by owner to update want address linked to the contract
+     * @return true if updation sucessful
+     */
     function updateWant(address _newWant) external onlyOwner returns (bool) {
         require (_newWant != address(0) && _newWant != want);
         want = _newWant;
     }
 
+    /**
+     * @dev called publically to get balance of a user in a perticular distribution
+     * @param user address of user
+     * @param distributionId Number of distribution 
+     * @return uint256 balance of user in the distribution with distribution Id
+     */
     function getBalanceByDistribution(address user, uint256 distributionId) public view returns (uint256) {
         return distributions[distributionId].fractions[user].mul(distributions[distributionId].totalFees).div(100).sub(distributions[distributionId].fundsWithdrawn[user]);
     }
 
+    /**
+     * @dev called publically to get balance of user
+     * @param user address of user
+     * @return uint256 total balance of user
+     */
     function getBalance(address user) external view returns (uint256) {
         uint256 totalAmount = 0;
         for(uint256 i = 0; i <= distributionIndex; i++) {
@@ -115,14 +158,30 @@ contract FeeDistributor is Ownable {
         return totalAmount;
     }
 
+    /**
+     * @dev called externally to view total fees in a distribution
+     * @param _distributionIndex the distribution index for which to check balances for
+     * @return uint256 total fees in distribution at a peerticular index
+     */
     function getTotalFees(uint256 _distributionIndex) external view returns (uint256) {
         return distributions[_distributionIndex].totalFees;
     }
 
+    /**
+     * @dev called externally to view user addresses in a distribution
+     * @param _distributionIndex the distribution index for a distribution to get users from
+     * @return address[] array of addresses of users
+     */
     function getUserAddresses(uint256 _distributionIndex) external view returns (address[] memory) {
         return distributions[_distributionIndex].users;
     }
 
+    /**
+     * @dev called externally to get user fraction in a distribution
+     * @param userAddress the user address to get fraction for
+     * @param _distributionIndex the distribution index for which to get fraction of user from
+     * @return uint256 fraction of user in a distribution
+     */
     function getUserFraction(
         uint256 _distributionIndex, 
         address userAddress
